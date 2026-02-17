facd709aad4648c00bd07f733cfd4b4f2923e5ef
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-17 16:43:26 -0600

    Fix constructor calls in Ageable subclasses

--- Entities.cs
+++ Entities.cs
@@ -15,7 +15,7 @@ public class Company : Ageable{
     private string TaxId;
     private decimal TaxRate;
 
-    public Company(string name, string taxId, int yearEstablished, decimal taxRate) : base(Year){
+    public Company(string name, string taxId, int yearEstablished, decimal taxRate) : base(yearEstablished){
         Name = name;
         TaxId = taxId;
         TaxRate = taxRate;
@@ -27,7 +27,7 @@ public class Person : Ageable{
     private string Ssn;
     private decimal TaxRate;
 
-    public Person(string name, string ssn, int birthYear, decimal taxRate) : base(Year){
+    public Person(string name, string ssn, int birthYear, decimal taxRate) : base(birthYear){
         Name = name;
         Ssn = ssn;
         TaxRate = taxRate;
@@ -38,11 +38,12 @@ public class Car : Ageable{
     private string Name;
     private string Vin;
 
-    public Car(string model, string vin, int year) : base(Year){
+    public Car(string model, string vin, int year) : base(year){
         Name = name;
         Vin = vin;
     }
 }
+
 
 
 
